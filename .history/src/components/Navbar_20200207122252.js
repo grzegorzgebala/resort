1628466a07451e9f22e7ae4a2513d7567f286c94
Grzegorz <grzegorz.gebala@yahoo.com>
2020-02-09 09:58:17 +0100

    Start with hero component

---
+++ .history/src/components/Navbar_20200207122252.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import {FaAlignRight} from 'react-icons/fa';
+
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                Hello from navbar
+            </div>
+        )
+    }
+}
