9a4aa2a7a4e957d4070ccc0fc253b0eea8317443
Grzegorz <grzegorz.gebala@yahoo.com>
2020-03-07 20:55:10 +0100

    Some filters implementation

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -33,6 +33,10 @@ export default function RoomFilter({ rooms }) {
             </option>
         );
     });
+    let people = getUnique(rooms, 'capacity');
+    people = people.map((item, index) => {
+        return <option key={index} value={item}>{item}</option>
+    })
     return (
         <section className="filter-container">
             <Title title="search rooms" />
@@ -52,6 +56,28 @@ export default function RoomFilter({ rooms }) {
                     
                 </div>
                 {/* end select type */}
+                {/* guests */}
+                <div className="form-group">
+                    <label htmlFor="capacity">Guests</label>
+                    <select 
+                        name="capacity" 
+                        id="capacity" 
+                        value={capacity} 
+                        className="form-control" 
+                        onChange={handleChange}
+                    >
+                    {people}
+                    </select>
+                </div>
+                {/* end guests */}
+                {/* room price */}
+                <div className="form-group">
+                    <label htmlFor="price">
+                        room price ${price}
+                    </label>
+                    <input type="range" name="price" min={minPrice} max={maxPrice} id="price" value={price} onChange={handleChange} className="form-control" />
+                </div>
+                {/* end of room price */}
             </form>
         </section>
     )
